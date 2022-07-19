b170321d0efc7cc41b68dddbe8ee97b7001157cb
aleksandr-wegele <aleksandr.wegele@gmail.com>
2022-07-19 18:19:37 +0300

    refactor: rename сlass

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className="wrapper clear">
       <header className="header">
         <div className="header__left">
-          <img widht={40} height={40} src="/img/logo_header.png" className="header__left-logo"/>
+          <img widht={40} height={40} src="/img/logo_header.png" className="header__left-logo" alt=""/>
           <div className="header__left-info">
-            <h3>React online shop</h3>
-            <p>the best shop</p>
+            <h3 className="header__left-info_headding">React online shop</h3>
+            <p className="header__left-info_description">the best shop</p>
           </div>
         </div>
         <div className="header__right">
           <ul className="header__right-list">
-            <li className="header__right-list_item">
-              <img widht={18} height={18} src="/img/cart_header.svg"/>
-              <span>1205 rub.</span>
+            <li className="header__right-list_item cart">
+              <img widht={18} height={18} src="/img/cart_header.svg" className="cart_img" alt=""/>
+              <span className="cart__count">1205 rub.</span>
             </li>
             <li>
-              <img widht={18} height={18} src="/img/favorite_header.svg"/>
+              <img widht={18} height={18} src="/img/favorite_header.svg" className="favorite" alt=""/>
             </li>
             <li>
-              <img widht={18} height={18} src="/img/union_header.svg"/>
+              <img widht={18} height={18} src="/img/union_header.svg" className="union" alt=""/>
             </li>
           </ul>
         </div>
