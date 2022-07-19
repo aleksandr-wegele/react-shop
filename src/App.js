aeaa1417fe0e4db474ddc7cfd8c6ff9267535b19
aleksandr-wegele <aleksandr.wegele@gmail.com>
2022-07-19 16:47:58 +0300

    init: start online-shop-react

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header>
+        <span>Logo</span>
+        <span>Cart</span>
+        <span>Log-in</span>
+      </header>
+      <main>
+        <section>
+          <div>slider</div>
+        </section>
+        <section>
+          <h1>Заголовки 1го уровня</h1>
+        </section>
+        <section>
+          <div>Сетка товаров
+            <div>Карточки</div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
