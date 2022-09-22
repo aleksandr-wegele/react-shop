56685a2787d58a1cab853d404539aae265b7d712
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-09-22 21:15:49 +0300

    feat: use props obj

--- src/components/content/ContentItem.js
+++ src/components/content/ContentItem.js
@@ -1,17 +1,15 @@
-
-
-function ContentItem(){
+function ContentItem(props){
 return (
         <div className="content__item">
           <div className="like">
             <img src="/img/no-like.svg" alt="no-like"/>
           </div>
-          <img  src="/img/sneakers/1.png" className="content__item_img" alt=""/>
-          <p className="content__item_headding">Мужские Кроссовки Nike Blazer Mid Suede</p>
+          <img  src={props.imgUrl} className="content__item_img" alt=""/>
+          <p className="content__item_headding">{props.title}</p>
           <div className="content__item_description">
             <div className="price__wrapper">
             <span className="content__item_description-price_head">Цена:</span>
-            <b className="content__item_description-price">12 999 руб.</b>
+            <b className="content__item_description-price">{props.price} руб.</b>
             </div>
             <img widht={11} height={11} src="/img/btn-plus.svg" className="content__item_description-btn plus" alt=""/>
           </div>
