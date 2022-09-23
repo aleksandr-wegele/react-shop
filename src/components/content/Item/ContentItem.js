b91b8f9a463d117861b9c8217adbdd0e29b4c810
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-09-23 19:53:16 +0300

    fix: url img

---
+++ src/components/content/Item/ContentItem.js
@@ -0,0 +1,27 @@
+import styles from './ContentItem.module.scss';
+import btnPlus from '../../img/btn-plus.svg';
+import noLike from '../../img/no-like.svg';
+
+function ContentItem(props){
+ console.log(props);
+  
+
+return (
+        <div className={styles.content__item}>
+          <div className={styles.like}>
+            <img src={noLike} alt="no-like"/>
+          </div>
+          <img  src={props.imgUrl} className={styles.content__item_img} alt=""/>
+          <p className={styles.content__item_headding}>{props.title}</p>
+          <div className={styles.content__item_description}>
+            <div className={styles.price__wrapper}>
+            <span className={styles.content__item_description_price_head}>Цена:</span>
+            <b className={styles.content__item_description_price}>{props.price} руб.</b>
+            </div>
+            <img widht={11} height={11} src={btnPlus} className={styles.content__item_description_btn} alt="plus" onClick={props.onClick}/>
+          </div>
+        </div>
+        )
+      }
+
+export default ContentItem 
