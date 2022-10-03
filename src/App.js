fea229eb6fe9808889ce1444b525ab934a16d897
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-10-03 23:28:05 +0300

    feat: open cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Header from "./components/Header";
 import MainContent from "./components/content/MainContent";
 import Drawer from "./components/Drawer";
+import React from "react";
 
 function App() {
+  const [cartOpened, setCartOpened] = React.useState(false);
+
   return (
     <div className="wrapper clear">
 
-      <Drawer/>
-      <Header/>
+      {cartOpened && <Drawer onClose={() => setCartOpened(false)}/>}
+      <Header onClickCart={() => setCartOpened(true)}/>
       <MainContent/>
+      
     </div>
   );
 }
