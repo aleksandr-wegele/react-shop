2b9e03a27482f8759179e6533f26a2f18f76346e
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-10-03 23:29:34 +0300

    fix: img

--- src/components/content/Item/ContentItem.js
+++ src/components/content/Item/ContentItem.js
@@ -1,24 +1,29 @@
+import React from 'react';
 import styles from './ContentItem.module.scss';
-import noLike from './img/no-like.svg'
+import noLike from './img/no-like.png'
 import btnPlus from './img/btn-plus.svg'
+import btnChek from './img/btn-check.svg';
 
 function ContentItem(props){
- console.log(props);
-  
+ const [isAdded, setIsAdded] = React.useState();
+
+ const onClickPlus = () => {
+  setIsAdded(!isAdded);
+ };
 
 return (
         <div className={styles.content__item}>
           <div className={styles.like}>
-            <img src={noLike} alt="no-like"/>
+            <img src={noLike} alt="no-like" onClick={props.onFavorite} className={styles.no_like}/>
           </div>
-          <img  src={props.imgUrl} className={styles.content__item_img} alt=""/>
+          <img  src={props.imgUrl} className={styles.content__item_img} alt="sneakers"/>
           <p className={styles.content__item_headding}>{props.title}</p>
           <div className={styles.content__item_description}>
             <div className={styles.price__wrapper}>
             <span className={styles.content__item_description_price_head}>Цена:</span>
             <b className={styles.content__item_description_price}>{props.price} руб.</b>
             </div>
-            <img widht={11} height={11} src={btnPlus} className={styles.content__item_description_btn} alt="plus" onClick={props.onClick}/>
+            <img src={isAdded ? btnChek : btnPlus} className={styles.content__item_description_btn} alt="plus" onClick={onClickPlus}/>
           </div>
         </div>
         )
