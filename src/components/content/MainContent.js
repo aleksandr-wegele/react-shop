6984bfc00946ebc7698b60a86b31607280f161bd
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-10-03 23:26:49 +0300

    feat: add check onclick

--- src/components/content/MainContent.js
+++ src/components/content/MainContent.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ContentItem from "./Item/ContentItem";
 
 
+
 function MainContent(){
   const [items, setItems] = React.useState([])
 
@@ -16,27 +17,29 @@ React.useEffect(() =>{
 
     return(
         <main className="main">
-      <div className="slider"></div>
-      <div className="content">
-        <div className="content__headding">
-          <h1 className="headding">Все кроссовки</h1>
-          <div className="search__block">
-            <img src="../img/search.svg" className="search_img" alt="search"/>
-            <input className="search__block_search" placeholder="Поиск..."/>
+          <div className="slider"></div>
+          <div className="content">
+            <div className="content__headding">
+              <h1 className="headding">Все кроссовки</h1>
+              <div className="search__block">
+                <img src="../img/search.svg" className="search_img" alt="search"/>
+                <input className="search__block_search" placeholder="Поиск..."/>
+              </div>
+            </div>
+            <div className="all_item">
+            {items.map((obj) => (
+              <ContentItem 
+              title={obj.title} 
+              price={obj.price} 
+              imgUrl={obj.imgUrl} 
+              onFavorite={() => console.log("добавили закладки")}
+              onBtnPlus={() => console.log("Нажали плюс")}
+              />
+            ))}
+            </div>
           </div>
-        </div>
-        <div className="all_item">
-        {arrContentItem.map((obj) => (
-          <ContentItem 
-          title={obj.title} 
-          price={obj.price} 
-          imgUrl={obj.imgUrl} 
-          onClick={() => console.log(obj)}
-          />
-        ))}
-        </div>
-      </div>
-      </main>
+       </main>
     )
 }
+
 export default MainContent
