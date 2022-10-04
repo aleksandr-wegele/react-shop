b5097ac426aeb6fbc8498973b404f0c9095e6a86
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-10-04 16:15:12 +0300

    feat: add modules scss

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,34 +1,36 @@
+import styles from './Drawer.module.scss'
+
 function Drawer ({onClose, items = []}){
     return(
-        <div className="overlay">
-        <div className="drawer">
-          <h2 className="drawer__name">Корзина <img onClick={onClose} src="/img/btn-remove.svg" alt="Remove" className="btn-remove"/></h2>
-          <div className="items">
+        <div className={styles.overlay}>
+        <div className={styles.drawer}>
+          <h2 className={styles.drawer__name}>Корзина <img onClick={onClose} src="/img/btn-remove.svg" alt="Remove" className={styles.btn_remove}/></h2>
+          <div className={styles.items}>
             {items.map((obj) => (
-              <div className="cartItem">
-              <img src={obj.imgUrl} alt="" className="cartItem_img" widht={70} height={70}/>
-              <div className="cartItem_info">
-                <p className="cartItem_info-description">{obj.title}</p>
-                <b className="cartItem_info-price">{obj.price} руб.</b>
+              <div className={styles.cartItem}>
+              <img src={obj.imgUrl} alt="Poster" className={styles.cartItem_img} widht={70} height={70}/>
+              <div className={styles.cartItem_info}>
+                <p className={styles.cartItem_info_description}>{obj.title}</p>
+                <b className={styles.cartItem_info_price}>{obj.price} руб.</b>
               </div>
-              <img src="/img/btn-remove.svg" alt="Remove" className="btn-remove"/>
+              <img src="/img/btn-remove.svg" alt="Remove" className={styles.btn_remove}/>
             </div>
             ))}
           </div>
 
-          <ul className="drawer__list">
-            <li className="drawer__list_item">
-              <span className="list__item_headding">Итого:</span>
-              <div className="list__item_dashed"></div>
-              <b  className="list__item_description">21 498 руб.</b>
+          <ul className={styles.drawer__list}>
+            <li className={styles.drawer__list_item}>
+              <span className={styles.list__item_headding}>Итого:</span>
+              <div className={styles.list__item_dashed}></div>
+              <b  className={styles.list__item_description}>21 498 руб.</b>
             </li>
-            <li className="drawer__list_item">
-              <span className="list__item_headding">Налог 5%:</span>
-              <div className="list__item_dashed"></div>
-              <b  className="list__item_description">1074 руб.</b>
+            <li className={styles.drawer__list_item}>
+              <span className={styles.list__item_headding}>Налог 5%:</span>
+              <div className={styles.list__item_dashed}></div>
+              <b  className={styles.list__item_description}>1074 руб.</b>
             </li>
           </ul>
-          <button className="drawer__btn">Оформить заказ <img src="/img/next.svg" alt="next" className="drawer__btn_next"/> </button>
+          <button className={styles.drawer__btn}>Оформить заказ <img src="/img/next.svg" alt="next" className={styles.drawer__btn_next}/> </button>
 
         </div>
       </div>
