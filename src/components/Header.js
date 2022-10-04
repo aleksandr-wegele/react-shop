c6ec3d3d047256fe0c02c63024ef48b19a25e2d6
aleksandr-wegele <109079074+aleksandr-wegele@users.noreply.github.com>
2022-10-04 16:15:00 +0300

    feat: add modules scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
+import styles from './Header.module.scss'
+
 function Header(props){
     return(
-        <header className="header">
-        <div className="header__left">
-          <img widht={40} height={40} src="/img/logo_header.png" className="header__left-logo" alt=""/>
-          <div className="header__left-info">
-            <h3 className="header__left-info_headding">React online shop</h3>
-            <p className="header__left-info_description">Лучший магазин кросовок</p>
+        <header className={styles.header}>
+        <div className={styles.header__left}>
+          <img widht={40} height={40} src="/img/logo_header.png" className={styles.header__left_logo} alt=""/>
+          <div className={styles.header__left_info}>
+            <h3 className={styles.header__left_info_headding}>React online shop</h3>
+            <p className={styles.header__left_info_description}>Лучший магазин кросовок</p>
           </div>
         </div>
-        <div className="header__right">
-          <ul className="header__right-list">
-            <li onClick={props.onClickCart} className="header__right-list_item cart">
-              <img widht={18} height={18} src="/img/cart_header.svg" className="cart_img" alt=""/>
-              <span className="cart__count">1205 руб.</span>
+        <div className={styles.header__right}>
+          <ul className={styles.header__right_list}>
+            <li onClick={props.onClickCart} className={styles.header__right_list_item + ' ' + styles.cart}>
+              <img widht={18} height={18} src="/img/cart_header.svg" className={styles.cart_img} alt=""/>
+              <span className={styles.cart__count}>1205 руб.</span>
             </li>
-            <li className="header__right-list_item favorite">
-              <img widht={18} height={18} src="/img/favorite_header.svg" className="favorite" alt=""/>
+            <li className={styles.header__right_list_item + ' ' + styles.favorite}>
+              <img widht={18} height={18} src="/img/favorite_header.svg" className={styles.favorite} alt=""/>
             </li>
-            <li className="header__right-list_item union">
-              <img widht={18} height={18} src="/img/union_header.svg" className="union" alt=""/>
+            <li className={styles.header__right_list_item + ' ' + styles.union}>
+              <img widht={18} height={18} src="/img/union_header.svg" className={styles.union} alt=""/>
             </li>
           </ul>
         </div>
